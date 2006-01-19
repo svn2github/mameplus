ca375c2e30f6edf15a4b23e8f77ceb62c90d876b
but <but@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2006-01-19 10:39:31 +0000

    fixed crash with YM2610 games such as neopong
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@160 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/sound/ay8910.c
+++ trunk/mamep/src/sound/ay8910.c
@@ -622,7 +622,8 @@ static void AY8910_init(struct AY8910 *PSG, int streams,
 		read8_handler portAread,read8_handler portBread,
 		write8_handler portAwrite,write8_handler portBwrite)
 {
-    sample_rate = clock/8;
+// causes crashes with YM2610 games - overflow?
+//	sample_rate = clock/8;
 
 	PSG->SampleRate = sample_rate;
 	PSG->PortAread = portAread;
