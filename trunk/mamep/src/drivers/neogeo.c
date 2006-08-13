c10b1faa735760027b941c636a1496339cf86c3b
flycboy <flycboy@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2006-08-13 21:49:22 +0000

    name fix - -!!
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@1196 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/drivers/neogeo.c
+++ trunk/mamep/src/drivers/neogeo.c
@@ -10085,7 +10085,7 @@ GAMEB( 2003, mslug5d,  mslug5,   neogeo, neogeo, neogeo,  mslug5d,  ROT0, "SNK /
 GAMEB( 2003, samsho5d, samsho5,  neogeo, neogeo, neogeo,  samsho5d, ROT0, "Yuki Enterprise / SNK Playmore", "Samurai Shodown V / Samurai Spirits Zero (decrypted C)", 0 ) // different program scrambling
 GAMEB( 2003, samsh5sd, samsh5sp, neogeo, neogeo, neogeo,  samsh5sd, ROT0, "Yuki Enterprise / SNK Playmore", "Samurai Shodown V Special / Samurai Spirits Zero Special (decrypted C)", 0 )
 GAMEB( 2004, lans2k4d, shocktr2, neogeo, neogeo, neogeo,  neogeo,   ROT0, "bootleg", "Lansquenet 2004 (Shock Troopers - 2nd Squad Bootleg / fully decrypted)", 0 )
-GAMEB( 2003, svcd,     svc,      neogeo, neogeo, neogeo,  svcd,     ROT0, "booleg", "SVC Chaos - SNK vs CAPCOM (MVS / decrypted C)",0 )
+GAMEB( 2003, svcd,     svc,      neogeo, neogeo, neogeo,  svcd,     ROT0, "bootleg", "SVC Chaos - SNK vs CAPCOM (MVS / decrypted C)",0 )
 GAMEB( 2001, jckeygpd, jockeygp, neogeo, neogeo, jockeygp,jckeygpd, ROT0, "Sun Amusement / BrezzaSoft", "Jockey Grand Prix (decrypted C)", 0 )
 GAME ( 2003, kf2k3pcd, kf2k3pcb,         neogeo, neogeo,  kf2k3pcd, ROT0, "SNK Playmore", "The King of Fighters 2003 (Japan, JAMMA PCB / decrypted C&Bios)" , 0 ) /* Encrypted Code & Sound */
 GAMEB( 2005, kogd,     kof97,    neogeo, neogeo, kog,     neogeo,   ROT0, "bootleg", "The King of gladiator (The King of Fighters '97 bootleg / fully decrypted)", 0 )
