1695a518efcd6b6bdc7a1fbe73a39b3aa4c912f0
but <but@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2006-09-23 00:52:58 +0000

    removed unused macros
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@1358 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/windows/osinline.h
+++ trunk/mamep/src/windows/osinline.h
@@ -36,7 +36,7 @@ INLINE int _vec_mult(int x, int y)
         mov eax, x
         imul y
         mov result, edx
-}
+    }
 
     return result;
 }
@@ -61,47 +61,4 @@ INLINE int _vec_mult(int x, int y)
 
 #endif /* PTR64 */
 
-
-/*-------------------------------------------------
-	TRANSPARENT UI MACROS
--------------------------------------------------*/
-
-#ifndef USE_SAMPLE_MACORS_FOR_TRANSPARENT_UI
-#define draw_transparent16_PALETTE(src,dst,y) \
-{ \
-	UINT8 r = ((UINT8 *)&adjusted_palette[*src])[2]; \
-	UINT8 g = ((UINT8 *)&adjusted_palette[*src])[1]; \
-	UINT8 b = ((UINT8 *)&adjusted_palette[*src])[0]; \
- \
-	r = ui_transparent_background[0][y << 8 | r]; \
-	g = ui_transparent_background[1][y << 8 | g]; \
-	b = ui_transparent_background[2][y << 8 | b]; \
- \
-	*dst = find_near_palette_by_index(get_colormap_index_by_rgb(r, g, b)); \
-}
-
-#define draw_transparent16_RGB15(src,dst,y) \
-{ \
-	UINT8 r = (*src >> 10); \
-	UINT8 g = (*src >> 5); \
-	UINT8 b = *src; \
- \
-	r = ui_transparent_background[0][y << 8 | r]; \
-	g = ui_transparent_background[1][y << 8 | g]; \
-	b = ui_transparent_background[2][y << 8 | b]; \
- \
-	*dst = r << 10 | g << 5 | b; \
-}
-
-#define draw_transparent32_RGB32(src,dst,y) \
-{ \
-	UINT8 r = ((UINT8 *)src)[2]; \
-	UINT8 g = ((UINT8 *)src)[1]; \
-	UINT8 b = ((UINT8 *)src)[0]; \
- \
-	((UINT8 *)dst)[2] = ui_transparent_background[0][y << 8 | r]; \
-	((UINT8 *)dst)[1] = ui_transparent_background[1][y << 8 | g]; \
-	((UINT8 *)dst)[0] = ui_transparent_background[2][y << 8 | b]; \
-}
-#endif /* USE_SAMPLE_MACORS_FOR_TRANSPARENT_UI */
 #endif /* __OSINLINE__ */
