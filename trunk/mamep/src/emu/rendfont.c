e0667d72b56c4d0bd140e8e33c4ddb4df983b6a9
but <but@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2007-02-14 10:46:26 +0000

    fixed font rendering
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@1699 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/emu/rendfont.c
+++ trunk/mamep/src/emu/rendfont.c
@@ -818,6 +818,7 @@ static void render_font_char_expand(render_font *font, render_font_char *ch)
 
 	/* allocate a new bitmap of the size we need */
 	ch->bitmap = bitmap_alloc(ch->bmwidth, font->height, BITMAP_FORMAT_ARGB32);
+	fillbitmap(ch->bitmap, MAKE_ARGB(0x00,0xff,0xff,0xff), NULL);
 
 	/* extract the data */
 	for (y = 0; y < ch->bmheight; y++)
