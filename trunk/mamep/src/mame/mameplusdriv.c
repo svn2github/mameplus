7ec05fa15ddf506f4fdd46c1acd14ad3aa609c06
kof2112 <kof2112@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2007-02-23 04:16:06 +0000

    - added Darkstalkers: The Night Warriors (USA 940705, Phoenix Edition)
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@1781 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/mame/mameplusdriv.c
+++ trunk/mamep/src/mame/mameplusdriv.c
@@ -77,6 +77,7 @@ const game_driver * const plusdrivers[] =
 	DRIVER( gwingjd )	/* 23/02/1999 (c) 1999 Takumi (Japan) Phoenix Edition */
 	DRIVER( 1944d )		/* 20/06/2000 (c) 2000 Eighting/Raizing (US) Phoenix Edition */
 	DRIVER( hsf2d )	/* 02/02/2004 (c) 2004 (Asia) Phoenix Edition */
+	DRIVER( dstlku1d )	/* 05/07/1994 (c) 1994 (Phoenix Edition, US 940705) */
 
 	DRIVER( kof96ep )	/* 0214 bootleg */
 	DRIVER( kof97pla )	/* 0232 (c) 2003 bootleg */
