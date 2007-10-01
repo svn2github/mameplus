8c05edd81654a9cf02fff05dc03fe5b176d120ee
but <but@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2007-10-01 05:32:00 +0000

    updated mame statistics
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@2423 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/mame/statistics.h
+++ trunk/mamep/src/mame/statistics.h
@@ -1,5 +1,6 @@
 static const char *stat_versions[] =
 {
+	"0.119u2    September 29th 2007",
 	"0.119u1    September 22nd 2007",
 	"0.119      September 13th 2007",
 	"0.118u6    September 11th 2007",
@@ -401,6 +402,7 @@ static const char *stat_versions[] =
 
 static const char *stat_history[] =
 {
+	"0.119u2    939    6817  +15",
 	"0.119u1    935    6802  +30",
 	"0.119      933    6772  +7",
 	"0.118u6    933    6765  +13",
@@ -803,6 +805,6 @@ static const char *stat_newgames[] =
 	"2004:   65      +  675",
 	"2005:   70      +  563",
 	"2006:   39      +  458",
-	"2007:   45      +  401",
+	"2007:   49      +  416",
 	0
 };
