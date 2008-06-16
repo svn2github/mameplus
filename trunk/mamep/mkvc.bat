90a636b76b258e875c36a5860c3cc4bcecc63204
emuman <emuman@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2008-06-16 07:58:29 +0000

    REL 0.125u5 fixed MSVC compiling problem for M68K DRC [JoshuaChang]
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@3391 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/mkvc.bat
+++ trunk/mamep/mkvc.bat
@@ -1,4 +1,6 @@
 call env
 call "%VS90COMNTOOLS%vsvars32.bat"
 mingw32-make MSVC_BUILD=1 PREFIX= MAXOPT= OPTIMIZE=ng obj/windows/mamep/emu/mconfig.o
+mingw32-make MSVC_BUILD=1 PREFIX= MAXOPT= OPTIMIZE=ng obj/windows/mamep/emu/cpu/m68000/d68kcpu.o
+mingw32-make MSVC_BUILD=1 PREFIX= MAXOPT= NO_FORCEINLINE=1 obj/windows/mamep/emu/cpu/m68000/d68kops.o
 mingw32-make MSVC_BUILD=1 PREFIX= MAXOPT=
