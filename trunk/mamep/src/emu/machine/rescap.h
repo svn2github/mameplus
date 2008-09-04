e3dc72e5b7c3a8ecec627a69fed223a57848f78d
sword <sword@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2008-09-04 20:07:04 +0000

    *[MAME] sync with 0.127u2 #2
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@3722 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/emu/machine/rescap.h
+++ trunk/mamep/src/emu/machine/rescap.h
@@ -2,11 +2,11 @@
 #define RC_MACROS_H
 
 /* Little helpers for magnitude conversions */
-#define RES_K(res) ((double)(res)*1e3)
-#define RES_M(res) ((double)(res)*1e6)
-#define CAP_U(cap) ((double)(cap)*1e-6)
-#define CAP_N(cap) ((double)(cap)*1e-9)
-#define CAP_P(cap) ((double)(cap)*1e-12)
+#define RES_K(res) ((double)(res) * 1e3)
+#define RES_M(res) ((double)(res) * 1e6)
+#define CAP_U(cap) ((double)(cap) * 1e-6)
+#define CAP_N(cap) ((double)(cap) * 1e-9)
+#define CAP_P(cap) ((double)(cap) * 1e-12)
 
 /*  vin --/\r1/\-- out --/\r2/\-- gnd  */
 #define RES_VOLTAGE_DIVIDER(r1, r2)		((double)(r2) / ((double)(r1) + (double)(r2)))
