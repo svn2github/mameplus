5807632f60b3e9c7ea8f397f49c440bf6a4bfd5b
sword <sword@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2008-10-22 08:17:29 +0000

    *kr.bat updated
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@3832 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/makelang/kr.bat
+++ trunk/mamep/makelang/kr.bat
@@ -1,4 +1,5 @@
 perl makemmo.pl kr 949
 mkdir ..\lang\ko_KR\
 move /y tmp\*.mmo ..\lang\ko_KR\
+@copy ..\makelang\data\kr\command.dat ..\lang\ko_KR\command.dat
 @copy ..\makelang\data\kr\history.dat ..\lang\ko_KR\history.dat
