141929d54d6e69a5b02aad26725ab17fecef99b7
sword <sword@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2009-05-06 20:30:05 +0000

    updated tw command.dat (20070920) [jong95] updated cn command.dat (20070920) [jong95] *updated cn history.dat (20090426) [maramail]
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@4208 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/makelang/cn.bat
+++ trunk/mamep/makelang/cn.bat
@@ -1,5 +1,5 @@
 perl makemmo.pl cn 936
 mkdir ..\lang\zh_CN\
 move /y tmp\*.mmo ..\lang\zh_CN\
-@copy ..\makelang\data\jp\command.dat ..\lang\zh_CN\command.dat
+@copy ..\makelang\data\cn\command.dat ..\lang\zh_CN\command.dat
 @copy ..\makelang\data\cn\history.dat ..\lang\zh_CN\history.dat
--- trunk/mamep/makelang/tw.bat
+++ trunk/mamep/makelang/tw.bat
@@ -1,4 +1,4 @@
 perl makemmo.pl tw 950
 mkdir ..\lang\zh_TW\
 move /y tmp\*.mmo ..\lang\zh_TW\
-@copy ..\makelang\data\jp\command.dat ..\lang\zh_TW\command.dat
+@copy ..\makelang\data\tw\command.dat ..\lang\zh_TW\command.dat
