ab8b6e5a0954540955f0794bdda2e82e79f8528d
kof2112 <kof2112@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2009-08-23 03:00:47 +0000

    *Revert to svn r4354
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@4355 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/mame/machine/gdcrypt.c
+++ trunk/mamep/src/mame/machine/gdcrypt.c
@@ -339,7 +339,7 @@ void naomi_game_decrypt(running_machine* machine, UINT64 key, UINT8* region, int
 
 	des_generate_subkeys (rev64(key), des_subkeys);
 
-//	#ifdef MAME_DEBUG
+	#ifdef MAME_DEBUG
 	/* save the original file */
 	{
 		FILE *fp;
@@ -352,7 +352,7 @@ void naomi_game_decrypt(running_machine* machine, UINT64 key, UINT8* region, int
 			fclose(fp);
 		}
 	}
-//	#endif
+	#endif
 
 	for(i=0;i<length;i+=8)
 	{
@@ -364,7 +364,7 @@ void naomi_game_decrypt(running_machine* machine, UINT64 key, UINT8* region, int
 	 	write_from_qword(region+i, ret);
 	}
 
-//	#ifdef MAME_DEBUG
+	#ifdef MAME_DEBUG
 	/* save the decrypted file */
 	{
 		FILE *fp;
@@ -377,6 +377,6 @@ void naomi_game_decrypt(running_machine* machine, UINT64 key, UINT8* region, int
 			fclose(fp);
 		}
 	}
-//	#endif
+	#endif
 }
 
