334cb073ba54b0840e537f4bc9b9c5571e3381c5
sword <sword@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2010-01-24 06:08:21 +0000

    *[MESS] sync with svn r7156
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@4560 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/mess/osd/windows/messres.h
+++ trunk/mamep/src/mess/osd/windows/messres.h
@@ -2,90 +2,69 @@
 // Microsoft Visual C++ generated include file.
 // Used by mess.rc
 //
-
-//mamep: conflict symbols with winui/resource.h
-#ifndef ID_FILE_LOADSTATE_NEWUI    
-#define ID_FILE_LOADSTATE_NEWUI         40095
-#endif /* !ID_FILE_LOADSTATE_NEWUI */
-
-#ifndef ID_FILE_EXIT_NEWUI
-#define ID_FILE_EXIT_NEWUI              40001
-#endif /* !ID_FILE_EXIT_NEWUI */
-
-#ifndef ID_HELP_ABOUT_NEWUI
-#define ID_HELP_ABOUT_NEWUI             40006
-#endif /* !ID_HELP_ABOUT_NEWUI */
-
-#ifndef IDC_PLAY_NEWUI
-#define IDC_PLAY_NEWUI                  1001
-#endif /* !IDC_PLAY_NEWUI */
-//mamep: end of conflict symbols with winui/resource.h
-
-
-//mamep: leave id from winui/resource.h
-//**#define ID_FILE_LOADSTATE_NEWUI     2101
-#define ID_FILE_SAVESTATE               2102
-#define ID_FILE_SAVESTATE_AS            2103
-#define ID_FILE_SAVESCREENSHOT          2104
-//**#define ID_FILE_EXIT_NEWUI          2105
-#define ID_EDIT_PASTE                   2106
-#define ID_OPTIONS_HIDEMENUBAR          2107
-#define ID_OPTIONS_KEYBOARD             2108
-#define ID_OPTIONS_CONFIGURATION        2109
-#define ID_OPTIONS_DIPSWITCHES          2110
-#define ID_OPTIONS_MISCINPUT            2111
-#define ID_OPTIONS_USEMOUSE             2112
-#define ID_OPTIONS_TOGGLEMENUBAR        2113
-#define ID_OPTIONS_SOFTRESET            2114
-#define ID_OPTIONS_HARDRESET            2115
-#define ID_OPTIONS_FRAMESKIP            2116
-#define ID_OPTIONS_PAUSE                2117
-#define ID_OPTIONS_JOYSTICKS            2118
-#define ID_OPTIONS_ANALOGCONTROLS       2119
-#define ID_OPTIONS_FULLSCREEN           2120
-#define ID_OPTIONS_PROFILER             2121
-#define ID_OPTIONS_TOGGLEFPS            2122
-#define ID_OPTIONS_DEBUGGER             2123
-#define ID_KEYBOARD_EMULATED            2124
-#define ID_KEYBOARD_NATURAL             2125
-#define ID_KEYBOARD_CUSTOMIZE           2126
-#define ID_VIDEO_ROTATE_0               2127
-#define ID_VIDEO_ROTATE_90              2128
-#define ID_VIDEO_ROTATE_180             2129
-#define ID_VIDEO_ROTATE_270             2130
-#define ID_THROTTLE_50                  2131
-#define ID_THROTTLE_100                 2132
-#define ID_THROTTLE_200                 2133
-#define ID_THROTTLE_500                 2134
-#define ID_THROTTLE_1000                2135
-#define ID_THROTTLE_UNTHROTTLED         2136
-#define ID_FRAMESKIP_AUTO               2137
-#define ID_DEVICES                      2138
-//**#define ID_HELP_ABOUT_NEWUI         2139
-#define ID_HELP_ABOUTSYSTEM             2140
-#define IDD_TAPEDIALOG                  2200
-#define IDI_ICON_CART                   2300
-#define IDI_ICON_HARD                   2301
-#define IDI_ICON_CASS                   2302
-#define IDI_ICON_FLOP                   2303
-#define IDI_ICON_PRIN                   2304
-#define IDI_ICON_SERL                   2305
-#define IDI_ICON_SNAP                   2306
-#define IDR_RUNTIME_MENU                2400
-#define IDC_SLIDER                      2400
-//**#define IDC_PLAY_NEWUI              2401
-#define IDC_STOP                        2402
-#define IDC_PAUSE                       2403
-#define IDC_CAPTION                     2404
-#define IDC_STATUS                      2405
+#define ID_FILE_LOADSTATE_NEWUI         3101
+#define ID_FILE_SAVESTATE               3102
+#define ID_FILE_SAVESTATE_AS            3103
+#define ID_FILE_SAVESCREENSHOT          3104
+#define ID_FILE_EXIT_NEWUI              3105
+#define ID_EDIT_PASTE                   3106
+#define ID_OPTIONS_HIDEMENUBAR          3107
+#define ID_OPTIONS_KEYBOARD             3108
+#define ID_OPTIONS_CONFIGURATION        3109
+#define ID_OPTIONS_DIPSWITCHES          3110
+#define ID_OPTIONS_MISCINPUT            3111
+#define ID_OPTIONS_USEMOUSE             3112
+#define ID_OPTIONS_TOGGLEMENUBAR        3113
+#define ID_OPTIONS_SOFTRESET            3114
+#define ID_OPTIONS_HARDRESET            3115
+#define ID_OPTIONS_FRAMESKIP            3116
+#define ID_OPTIONS_PAUSE                3117
+#define ID_OPTIONS_JOYSTICKS            3118
+#define ID_OPTIONS_ANALOGCONTROLS       3119
+#define ID_OPTIONS_FULLSCREEN           3120
+#define ID_OPTIONS_PROFILER             3121
+#define ID_OPTIONS_TOGGLEFPS            3122
+#define ID_OPTIONS_DEBUGGER             3123
+#define ID_KEYBOARD_EMULATED            3124
+#define ID_KEYBOARD_NATURAL             3125
+#define ID_KEYBOARD_CUSTOMIZE           3126
+#define ID_VIDEO_ROTATE_0               3127
+#define ID_VIDEO_ROTATE_90              3128
+#define ID_VIDEO_ROTATE_180             3129
+#define ID_VIDEO_ROTATE_270             3130
+#define ID_THROTTLE_50                  3131
+#define ID_THROTTLE_100                 3132
+#define ID_THROTTLE_200                 3133
+#define ID_THROTTLE_500                 3134
+#define ID_THROTTLE_1000                3135
+#define ID_THROTTLE_UNTHROTTLED         3136
+#define ID_FRAMESKIP_AUTO               3137
+#define ID_DEVICES                      3138
+#define ID_HELP_ABOUT_NEWUI             3139
+#define ID_HELP_ABOUTSYSTEM             3140
+#define IDD_TAPEDIALOG                  3500
+#define IDI_ICON_CART                   3200
+#define IDI_ICON_HARD                   3201
+#define IDI_ICON_CASS                   3202
+#define IDI_ICON_FLOP                   3203
+#define IDI_ICON_PRIN                   3204
+#define IDI_ICON_SERL                   3205
+#define IDI_ICON_SNAP                   3206
+#define IDR_RUNTIME_MENU                3300
+#define IDC_SLIDER                      3300
+#define IDC_PLAY_NEWUI                  3301
+#define IDC_STOP                        3302
+#define IDC_PAUSE                       3303
+#define IDC_CAPTION                     3304
+#define IDC_STATUS                      3305
 
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        2141
-#define _APS_NEXT_COMMAND_VALUE         40008
-#define _APS_NEXT_CONTROL_VALUE         6006
-#define _APS_NEXT_SYMED_VALUE           2101
+#define _APS_NEXT_RESOURCE_VALUE        3129
+#define _APS_NEXT_COMMAND_VALUE         40108
+#define _APS_NEXT_CONTROL_VALUE         3306
+#define _APS_NEXT_SYMED_VALUE           100
 #endif
 #endif
