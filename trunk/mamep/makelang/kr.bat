8fb7365c62a25c9f64b7b5cf3529913d79ba01d7
sword <sword@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2010-09-20 14:11:06 +0000

    updated kr command.dat & history.dat & story.dat [gyunjadan]
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@4767 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/makelang/kr.bat
+++ trunk/mamep/makelang/kr.bat
@@ -3,3 +3,4 @@ mkdir ..\lang\ko_KR\
 move /y tmp\*.mmo ..\lang\ko_KR\
 @copy ..\makelang\data\kr\command.dat ..\lang\ko_KR\command.dat
 @copy ..\makelang\data\kr\history.dat ..\lang\ko_KR\history.dat
+@copy ..\makelang\data\kr\story.dat ..\lang\ko_KR\story.dat
