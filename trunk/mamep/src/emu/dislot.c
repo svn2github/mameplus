7f4c35f4a24b261d9ddcd264e89dbec61cf1b1ab
yuifan <yuifan@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2011-06-30 23:49:12 +0000

    * [MESS] Sync with MESS (rev 12129)
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@4890 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/emu/dislot.c
+++ trunk/mamep/src/emu/dislot.c
@@ -17,19 +17,20 @@ device_slot_interface::~device_slot_interface()
 }
 
 
-void device_slot_interface::static_set_slot_info(device_t &device, const slot_interface *slots_info, const char *default_card)
+void device_slot_interface::static_set_slot_info(device_t &device, const slot_interface *slots_info, const char *default_card,const input_device_default *default_input)
 {
 	device_slot_interface *slot;
 	if (!device.interface(slot))
 		throw emu_fatalerror("set_default_slot_card called on device '%s' with no slot interface", device.tag());
-
+	
 	slot->m_slot_interfaces = slots_info;
 	slot->m_default_card = default_card;
+	slot->m_input_defaults = default_input;
 }
 
 device_t* device_slot_interface::get_card_device()
 {
-	const char *subtag = device().machine().options().value(device().tag());
+	const char *subtag = device().machine().options().value(device().tag());	
 	device_t *dev = NULL;
 	if (subtag) {
 		device_slot_card_interface *intf = NULL;
