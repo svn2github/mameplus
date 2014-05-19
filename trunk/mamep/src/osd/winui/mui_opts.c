a23bf999d76117f92e892aa795817d81abdc0cdd
xvi <xvi@e0215e0a-1e64-f442-9f1d-d2f61a7f4648>
2014-05-19 22:37:00 +0000

    *[MAMEUI] revert mui_opts.c again
    git-svn-id: https://mameplus.svn.sourceforge.net/svnroot/mameplus@5154 e0215e0a-1e64-f442-9f1d-d2f61a7f4648

--- trunk/mamep/src/osd/winui/mui_opts.c
+++ trunk/mamep/src/osd/winui/mui_opts.c
@@ -3009,7 +3009,7 @@ void SaveDefaultOptions(void)
 {
 	char buffer[MAX_PATH];
 	GetGlobalOptionsFileName(buffer, ARRAY_LENGTH(buffer));
-//  SaveSettingsFile(global, NULL, buffer);
+	SaveSettingsFile(global, NULL, buffer);
 }
 
 const char * GetVersionString(void)
@@ -3246,7 +3246,7 @@ void save_options(OPTIONS_TYPE opt_type, windows_options &opts, int game_num)
 		// Don't try to save a null global options file,  or it will be erased.
 		//if (NULL == opts)
 			//return;
-		//global = opts;
+		global = opts;
 		filename.cpy(emulator_info::get_configname());
 	} else if (opt_type == OPTIONS_VECTOR)
 	{
